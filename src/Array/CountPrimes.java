7ead70abda36773aef0c5701269caf6fae20c57a
Vinit Gupta <vinitman35@gmail.com>
2021-09-15 20:00:22 +0530

    create : Solution to Leetcode Q-204 count Primes

--- src/Array/CountPrimes.java
+++ src/Array/CountPrimes.java
@@ -1,13 +1,35 @@
 package Array;
 
 public class CountPrimes {
-    public static int countPrimes(int n) {
-        int c = 0;
+//    public static int countPrimes(int n) {
+//        int c = 0;
+//        if(n<=1) return 0;
+//        for(int i=2;i<n;i++){
+//            if(isPrime(i)) c++;
+//        }
+//        return c;
+//    }
+
+    public static int countPrimes(int n){
         if(n<=1) return 0;
-        for(int i=2;i<n;i++){
-            if(isPrime(i)) c++;
+        boolean [] primes = new boolean[n];
+        for(int i=0;i<n;i++){
+            primes[i] = true;
         }
-        return c;
+        primes[0] = false;
+        primes[1] = false;
+        for(int i=2;i<n;i++){
+            if(primes[i]){
+                for(int j=2;i*j<n;j++){
+                    primes[i*j]  = false;
+                }
+            }
+        }
+        int c = 0;
+        for(int i=0;i<n;i++){
+            if(primes[i]) c++;
+        }
+        return  c;
     }
 
     public static boolean isPrime(int n){
@@ -20,7 +42,7 @@ public class CountPrimes {
     }
 
     public static void main(String[] args) {
-        int count = countPrimes(0);
+        int count = countPrimes(100);
         System.out.println("No. of Prime Nos. Less Than 10 : "+count);
     }
 }

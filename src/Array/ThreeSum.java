6403fca92b56606a2e711629c2e9ca30e2173e57
Vinit Gupta <vinitman35@gmail.com>
2021-09-22 10:59:38 +0530

    create : Solution to Leetcode Q-15 Three Sum (Better Approach)

--- src/Array/ThreeSum.java
+++ src/Array/ThreeSum.java
@@ -1,6 +1,7 @@
 package Array;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -36,10 +37,37 @@ public class ThreeSum {
 //        return ts;
 //    }
 
-
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ts = new ArrayList<List<Integer>>();
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            int j=i+1,k=nums.length-1;
+            int tar = -nums[i];
+            while(j<k){
+                if(nums[j]+nums[k] == tar){
+                    List<Integer> s = new ArrayList<Integer>();
+                    s.add(nums[i]);
+                    s.add(nums[j]);
+                    s.add(nums[k]);
+                    boolean canAdd = true;
+                    for(int l=0;l<ts.size();l++){
+                        if(s.equals(ts.get(l))) {
+                            canAdd=false;
+                            break;
+                        }
+                    }
+                    if(canAdd)
+                    ts.add(s);
+                }
+                if((nums[j]+nums[k]) > tar) k--;
+                else j++;
+            }
+        }
+        return ts;
+    }
 
     public static void main(String[] args) {
-        int [] nums = {-1,0,1,2,-1,-4};
+        int [] nums = {0,0,0,0};
         List<List<Integer>> ts = threeSum(nums);
         System.out.println("Three Sum Combinations : ");
         System.out.println(ts);

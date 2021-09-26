116c24dc26e48a25d7d8a4ab4352c96f4881241e
Vinit Gupta <vinitman35@gmail.com>
2021-09-26 11:31:54 +0530

    create : O(N) solution for Leetcode Q-49 Group Anagrams

---
+++ src/Strings/GroupAnagrams.java
@@ -0,0 +1,41 @@
+package Strings;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class GroupAnagrams {
+    static String sortString(String s){
+        char [] ch = s.toCharArray();
+        Arrays.sort(ch);
+        return String.valueOf(ch);
+    }
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> anagrams = new ArrayList<>();
+        String [] sorted = new String[strs.length];
+        for(int i=0;i<strs.length;i++){
+            sorted[i] = sortString(strs[i]);
+        }
+        boolean [] visited = new boolean[strs.length];
+        for(int i=0;i<sorted.length;i++){
+            List<String> s = new ArrayList<>();
+            if(visited[i]) continue;
+            s.add(strs[i]);
+            visited[i] = true;
+            for(int j=i+1;j<sorted.length;j++){
+                if(!visited[j] && sorted[i].equals(sorted[j]) ) {
+                    s.add(strs[j]);
+                    visited[j] = true;
+                }
+            }
+            anagrams.add(s);
+        }
+        return anagrams;
+    }
+
+    public static void main(String[] args) {
+        String [] s = {"a"};
+        List<List<String>> anagrams = groupAnagrams(s);
+        System.out.println(anagrams);
+    }
+}

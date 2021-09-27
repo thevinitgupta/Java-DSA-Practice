fd45d994b113d130ed40874855591f971fe86203
Vinit Gupta <vinitman35@gmail.com>
2021-09-27 10:44:58 +0530

    implement linked list

--- src/LinkedLists/MyLinkedList.java
+++ src/LinkedLists/MyLinkedList.java
@@ -1,5 +1,17 @@
 package LinkedLists;
 
 public class MyLinkedList {
-
+    public Node head;
+    public void add(int val){
+        Node newNode = new Node(val);
+        if(head==null){
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while(temp.next!=null){
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
 }

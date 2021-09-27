023fa805651110f1f34aefa4027fb163f398fee7
Vinit Gupta <vinitman35@gmail.com>
2021-09-27 10:44:35 +0530

    implement node of linked list

---
+++ src/LinkedLists/Node.java
@@ -0,0 +1,11 @@
+package LinkedLists;
+
+public class Node {
+        public Node next;
+        public int data;
+        Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+}
--- src/POTD/MiddleElementOfLinkedList.java
+++ src/POTD/MiddleElementOfLinkedList.java
@@ -1,7 +1,11 @@
 package POTD;
 
+import LinkedLists.MyLinkedList;
+import LinkedLists.Node;
 
 public class MiddleElementOfLinkedList {
+
+
     static int getMiddle(Node head)
     {
         // Your code here.
@@ -12,5 +16,15 @@ public class MiddleElementOfLinkedList {
             if(sec.next!=null) sec = sec.next;
         }
         return first.data;
+    }
+
+    public static void main(String[] args) {
+        MyLinkedList ll = new MyLinkedList();
+        ll.add(1);
+        ll.add(2);
+        ll.add(3);
+        ll.add(4);
+        ll.add(5);
+        System.out.println(getMiddle(ll.head));
     }
 }

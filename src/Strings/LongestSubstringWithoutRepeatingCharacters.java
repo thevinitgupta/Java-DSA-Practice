bda92afb94edf9c800b8ab20df391dd01869ffc9
Vinit Gupta <vinitman35@gmail.com>
2021-09-28 17:21:49 +0530

    update : O(N) solution for Leetcode Q-3 Longest Substring without Repeating Character

--- src/Strings/LongestSubstringWithoutRepeatingCharacters.java
+++ src/Strings/LongestSubstringWithoutRepeatingCharacters.java
@@ -3,22 +3,42 @@ package Strings;
 import java.util.Arrays;
 
 public class LongestSubstringWithoutRepeatingCharacters {
+//    public static int lengthOfLongestSubstring(String s) {
+//        int [] c = new int[256];
+//        Arrays.fill(c,-1);
+//        int i=0,j=0;
+//        int max = 0;
+//        while(j<s.length()){
+//            int l = (int)s.charAt(j);
+//            if(c[l]!=-1){
+//                max = Math.max(max,(j-i));
+//                for(int k=i;k<j;k++){
+//                    c[(int)s.charAt(k)] = -1;
+//                }
+//                i = j;
+//            }
+//            else {
+//                c[l] = j;
+//                j++;
+//            }
+//        }
+//        return max;
+//    }
+
     public static int lengthOfLongestSubstring(String s) {
-        int [] c = new int[256];
-        Arrays.fill(c,-1);
         int i=0,j=0;
         int max = 0;
-        while(j<s.length()){
-            int l = (int)s.charAt(j);
-            if(c[l]!=-1){
-                max = Math.max(max,(j-i));
-                for(int k=i;k<j;k++){
-                    c[(int)s.charAt(k)] = -1;
-                }
-                i = j;
+        int [] c = new int[256];
+        while(i<s.length() && j<s.length()){
+            int letter = s.charAt(j);
+            if(c[letter] != 0){
+                int let2 = s.charAt(i);
+                c[let2] = 0;
+                i++;
             }
             else {
-                c[l] = j;
+                c[letter] = 1;
+                max = Math.max(max,(j-i)+1);
                 j++;
             }
         }
@@ -26,7 +46,7 @@ public class LongestSubstringWithoutRepeatingCharacters {
     }
 
     public static void main(String[] args) {
-        int max = lengthOfLongestSubstring("abcdeeaaabc");
+        int max = lengthOfLongestSubstring("pwwkew");
         System.out.println("Max non repeating : "+max);
     }
 }

e5185bb39df4e9189d82088ed40c66f084dd3ddb
Vinit Gupta <vinitman35@gmail.com>
2021-10-05 10:51:58 +0530

    create : O(N) solution for Leetcode Q-1047 Remove All ADjacent Duplicates from String (Faster single iteration)

--- src/Strings/RemoveAllAdjacentDuplicatesFromString.java
+++ src/Strings/RemoveAllAdjacentDuplicatesFromString.java
@@ -3,41 +3,55 @@ package Strings;
 import java.util.Stack;
 
 public class RemoveAllAdjacentDuplicatesFromString {
+//    public static String removeDuplicates(String s) {
+//        if(s.length()<=1) return s;
+//        Stack<Character> letters = new Stack<>();
+//        letters.push(s.charAt(0));
+//        int i=1;
+//        while(i<s.length()){
+//            if(letters.isEmpty()){
+//                letters.push(s.charAt(i));
+//            }
+//            else if(letters.peek() == s.charAt(i)){
+//                letters.pop();
+//            }
+//            else {
+//                letters.push(s.charAt(i));
+//            }
+//            i++;
+//        }
+//        StringBuilder str = new StringBuilder();
+//        while(!letters.isEmpty()){
+//            str.insert(0,letters.pop());
+//        }
+//        return str.toString();
+//    }
+//    static String reverseString(String s){
+//        if(s.length()<=1) return s;
+//        int i=0,j=s.length()-1;
+//        char [] ch = s.toCharArray();
+//        while(i<j){
+//            char temp = ch[i];
+//            ch[i] = ch[j];
+//            ch[j] = temp;
+//            i++;
+//            j--;
+//        }
+//        return String.valueOf(ch);
+//    }
+
+
+    //faster single pass solution
     public static String removeDuplicates(String s) {
-        if(s.length()<=1) return s;
-        Stack<Character> letters = new Stack<>();
-        letters.push(s.charAt(0));
-        int i=1;
-        while(i<s.length()){
-            if(letters.isEmpty()){
-                letters.push(s.charAt(i));
+        StringBuilder sb = new StringBuilder();
+        for (char ch: s.toCharArray()) {
+            if ( sb.length() != 0 && sb.charAt(sb.length() - 1) == ch ) {
+                sb.deleteCharAt(sb.length() - 1);
+            } else {
+                sb.append(ch);
             }
-            else if(letters.peek() == s.charAt(i)){
-                letters.pop();
-            }
-            else {
-                letters.push(s.charAt(i));
-            }
-            i++;
         }
-        StringBuilder str = new StringBuilder();
-        while(!letters.isEmpty()){
-            str.insert(0,letters.pop());
-        }
-        return str.toString();
-    }
-    static String reverseString(String s){
-        if(s.length()<=1) return s;
-        int i=0,j=s.length()-1;
-        char [] ch = s.toCharArray();
-        while(i<j){
-            char temp = ch[i];
-            ch[i] = ch[j];
-            ch[j] = temp;
-            i++;
-            j--;
-        }
-        return String.valueOf(ch);
+        return sb.toString();
     }
     public static void main(String[] args) {
         String str = removeDuplicates("dabbacd");

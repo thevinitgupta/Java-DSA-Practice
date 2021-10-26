caaff30183196d9bb51b2e47b1aab88637a1a6f2
Vinit Gupta <vinitman35@gmail.com>
2021-10-26 20:33:15 +0530

    create : O(N) faster than 100% Solution for Leetcode Q-141 Linked List Cycle

--- src/LinkedLists/LinkedListCycle.java
+++ src/LinkedLists/LinkedListCycle.java
@@ -3,13 +3,28 @@ package LinkedLists;
 import java.util.HashMap;
 
 public class LinkedListCycle {
+
+    //O(N) time and Space Solution
+//    public boolean hasCycle(ListNode head) {
+//        HashMap<ListNode, Boolean> map = new HashMap<>();
+//        ListNode temp = head;
+//        while(temp!=null){
+//            if(map.containsKey(temp)) return true;
+//            map.put(temp,true);
+//            temp = temp.next;
+//        }
+//        return false;
+//    }
+
+    //faster than 100% solution with O(1) space
     public boolean hasCycle(ListNode head) {
-        HashMap<ListNode, Boolean> map = new HashMap<>();
-        ListNode temp = head;
-        while(temp!=null){
-            if(map.containsKey(temp)) return true;
-            map.put(temp,true);
-            temp = temp.next;
+        if(head==null || head.next==null) return false;
+        ListNode slow = head,fast = head.next.next;
+        while(fast!=null && slow!=null){
+            if(fast==slow) return true;
+            slow = slow.next;
+            fast = fast.next;
+            if(fast!=null) fast = fast.next;
         }
         return false;
     }

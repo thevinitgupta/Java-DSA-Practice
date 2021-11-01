7fb7f2b875a526120ec06d697597643e1106e6c1
Vinit Gupta <vinitman35@gmail.com>
2021-11-01 10:30:18 +0530

    create : O(N) Solution faster than 100% for Leetcode Q-430 Flatten Multi Level Doubly Linked List

--- src/DoublyLinkedLists/FlattenMultiLevelDoublyLinkedList.java
+++ src/DoublyLinkedLists/FlattenMultiLevelDoublyLinkedList.java
@@ -7,11 +7,11 @@ class Node {
 }
 
 public class FlattenMultiLevelDoublyLinkedList {
-    public Node flatten(Node head) {
-        if(head==null || head.next==null && head.child==null) return head;
-        Node temp = head,next = head.next;
+    public static Node getLast(Node head){
+        Node temp = head,prev= null;
         while(temp!=null){
             if(temp.child!=null){
+                Node next = temp.next;
                 Node childHead = temp.child;
                 Node childEnd = getLast(childHead);
                 childEnd.next = next;
@@ -19,19 +19,33 @@ public class FlattenMultiLevelDoublyLinkedList {
                 temp.next = temp.child;
                 temp.child = null;
                 if(next!=null)
-                next.prev = childEnd;
+                    next.prev = childEnd;
+                temp = childEnd;
+            }
+            prev = temp;
+            temp = temp.next;
+        }
+        return prev;
+    }
+    public Node flatten(Node head) {
+        if(head==null) return head;
+        Node temp = head,next = head.next;
+        while(temp!=null){
+            if(temp.child!=null){
+                Node childHead = temp.child;
+                Node childEnd = getLast(childHead);
+                if(childEnd!=null)
+                    childEnd.next = next;
+                childHead.prev = temp;
+                temp.next = temp.child;
+                temp.child = null;
+                if(next!=null)
+                    next.prev = childEnd;
                 temp = childEnd;
             }
             temp = temp.next;
-            next = next.next;
+            next = next==null ? null : next.next;
         }
         return head;
-    }
-    public static Node getLast(Node head){
-        Node temp = head;
-        while(temp.next!=null){
-            temp = temp.next;
-        }
-        return temp;
     }
 }

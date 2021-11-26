bf70bb3b8a80f0dad8f0ef05f37f19a82ca9d631
Vinit Gupta <vinitman35@gmail.com>
2021-11-26 22:32:29 +0530

    create :POTD by GeeksForGeeks Day-72 Money Distributions

---
+++ src/Array/RemoveDuplicateCharacters.java
@@ -0,0 +1,15 @@
+package Array;
+
+import java.util.*;
+
+public class RemoveDuplicateCharacters {
+
+    //[a,v,a,b,c,d,v,a,d] => [b,c,v,a,d]
+    public static char[] removeDuplicates(char[] arg){
+        ArrayList<Character> arr = new ArrayList<>();
+        HashMap<Character,Integer> map = new HashMap<>();
+        int n = arg.length;
+        int k=0;
+        return arg;
+    }
+}
---
+++ src/POTD/MoneyDistributions.java
@@ -0,0 +1,43 @@
+package POTD;
+
+public class MoneyDistributions {
+        public long power(long a,long b , long mod){
+
+            long ans = 1L;
+            while(b>0){
+                if((b&1)!=0 ){
+                    ans = (ans*a)%mod;
+                }
+                a = (a*a)%mod;
+                b>>=1;
+            }
+            return ans;
+        }
+
+        public int totalWays(int N, int K)
+        {
+            // Code here
+            if(K>N) return 0;
+            if(K==N) return 1;
+
+            return nCr(N-1,K-1);
+        }
+
+        public int nCr(int N,int R){
+
+            long res= 1L;
+            //long deno=1L;
+            if(R > N-R){
+                R = N-R;
+            }
+            long mod =(long) 1e9+7;
+            for(int i=0;i<R;i++){
+
+                res = (res *(N-i))%mod;
+                res= res*power(i+1 , mod-2 ,mod)%mod;
+
+            }
+
+            return (int)res;
+        }
+}

5551568d154df262cd963bb2da36c3104d9543d0
Vinit Gupta <vinitman35@gmail.com>
2021-11-30 18:23:31 +0530

    create :POTD by GeeksForGeeks Day-75 Shortest Range in BST

--- src/POTD/ShortestRangeInBST.java
+++ src/POTD/ShortestRangeInBST.java
@@ -1,6 +1,7 @@
 package POTD;
-
 import Trees.Node;
+
+import java.util.*;
 
 class Range {
     int x ,y;
@@ -15,12 +16,95 @@ class Range {
 }
 
 public class ShortestRangeInBST {
+//    HashMap<Integer, ArrayList<Integer>> map;
+//            int maxLevel = 0;
+//    public Range shortestRange(Node root) {
+//        // Your code goes here
+//        Range r = new Range();
+//        if(root.left==null && root.right==null) {
+//            r.x = root.data;
+//            r.y = root.data;
+//            return r;
+//        }
+//        map = new HashMap<>();
+//        maxLevel = 0;
+//        getLevelOrder(root,0);
+//        System.out.println(map);
+//        return r;
+//    }
+//    public void getLevelOrder(Node root,int level){
+//        if(root==null) return;
+//        if(level>maxLevel) maxLevel = level;
+//        ArrayList<Integer> list = new ArrayList<>();
+//        if(map.containsKey(level)){
+//            list = map.get(level);
+//        }
+//        list.add(root.data);
+//        getLevelOrder(root.left,level+1);
+//        getLevelOrder(root.right,level+1);
+//    }
+
     public Range shortestRange(Node root) {
-        // Your code goes here
-        Range r = new Range();
-        if(root.left==null && root.right==null) {
-            r.x = root.data;
-            r.y = root.data;
+        Map<Integer,List<Integer>> map = new HashMap<Integer,List<Integer>>();
+        levelOrderTraversal(root,1,map);
+
+        List<List<Integer>> level = new ArrayList<List<Integer>>();
+
+        int i=1;
+        while (map.get(i) != null) {
+            level.add(map.get(i));
+            i++;
         }
+        return smallestRange(level);
+    }
+    Map<Integer,List<Integer>> levelOrderTraversal(Node node, int level, Map<Integer,List<Integer>> map) {
+        if (node == null)
+            return map;
+
+        List<Integer> list = map.getOrDefault(level, new ArrayList<Integer>());
+        list.add(node.data);
+
+        map.put(level, list);
+
+        levelOrderTraversal(node.left, level+1, map);
+        levelOrderTraversal(node.right, level+1, map);
+
+        return map;
+    }
+    public Range smallestRange(List<List<Integer>> nums) {
+        if(nums.isEmpty()) return new Range(0,0);
+
+        Range range = new Range(0, Integer.MAX_VALUE);
+
+        PriorityQueue<LinkedList<Integer>> minHeap = new PriorityQueue<>((a, b) -> (a.peek() - b.peek()));
+        PriorityQueue<LinkedList<Integer>> maxHeap = new PriorityQueue<>((a, b) -> (b.peek() - a.peek()));
+
+        for(List<Integer> list: nums) {
+            LinkedList<Integer> linkedList = new LinkedList<>(list);
+            minHeap.add(linkedList);
+            maxHeap.add(linkedList);
+        }
+
+        while(minHeap.size() == nums.size()) {
+            LinkedList<Integer> minList = minHeap.poll();
+            LinkedList<Integer> maxList = maxHeap.poll();
+
+            int min = minList.getFirst();
+            int max = maxList.getFirst();
+
+            minList.removeFirst();
+
+            if(minList.size() > 0) {
+                minHeap.add(minList);
+                maxHeap.add(maxList);
+                if(!maxList.equals(minList)) {
+                    maxHeap.remove(minList);
+                    maxHeap.add(minList);
+                }
+            }
+            if((range.y - range.x) > (max - min))
+                range = new Range(min, max);
+        }
+        return range;
     }
 }
--- src/Strings/ReverseSubStringsBetweenParanthesisPairs.java
+++ src/Strings/ReverseSubStringsBetweenParanthesisPairs.java
@@ -1,8 +1,54 @@
 package Strings;
 
+import java.util.Stack;
+
 public class ReverseSubStringsBetweenParanthesisPairs {
     public static String reverseParentheses(String s) {
-        return editString(s);
+        StringBuilder str = new StringBuilder(s);
+        Stack<Integer> st = new Stack<>();
+        int i=0,n=s.length();
+        while(i<str.length()){
+            System.out.println(str);
+            System.out.println(i);
+            if(str.charAt(i)=='(') {
+                st.push(i);
+                i++;
+            }
+            else if(str.charAt(i)==')'){
+                String rev = revEditString(str.substring(st.peek(),i));
+                System.out.println("rev -> "+rev);
+                System.out.println("stack top -> "+st.peek());
+                str.replace(st.pop(),i+1,rev);
+                if(i<str.length()-1 && str.charAt(i-1)==')') i--;
+            }
+            else {
+                i++;
+            }
+        }
+        i=0;
+        while(str.charAt(i)=='('){
+            str.deleteCharAt(i);
+            i++;
+        }
+        return str.toString();
+       // return editString(s);
+    }
+// failing - > "((eqk((h))))"
+    //"((eqk((h))))"
+    public static String revEditString(String s){
+        String str = "";
+        System.out.println("TO reverse : "+s);
+        int j=s.length()-1,n=s.length();
+        while(s.charAt(j)==')'){
+            n--;
+            j--;
+        }
+        int i=1;
+        while(i<n){
+            str = s.charAt(i)+str;
+            i++;
+        }
+        return str;
     }
     public static String editString(String s){
         int j=0;
@@ -34,7 +80,7 @@ public class ReverseSubStringsBetweenParanthesisPairs {
     }
 
     public static void main(String[] args) {
-        String reversed = reverseParentheses("a(bcdefghijkl(mno)p)q");
+        String reversed = reverseParentheses("((eqk((h))))");
         System.out.println(reversed);
     }
 }

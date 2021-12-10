efcdb9f1cfec51d2c30bd63c295e515b67bd9957
Vinit Gupta <vinitman35@gmail.com>
2021-12-10 12:34:50 +0530

    create :Solution for leetcode Q-5 Longest Palindrome Substring

--- src/Strings/LongestPalindromicSubstring.java
+++ src/Strings/LongestPalindromicSubstring.java
@@ -3,6 +3,40 @@ package Strings;
 import java.util.Arrays;
 
 public class LongestPalindromicSubstring {
+
+    //leetcode Q-5 incomplete recursive solution
+    public String longestPalindromeSubstring(String s) {
+        int n = s.length();
+        if(n<=1) return s;
+        int i=n/2 - 1,j=n/2;
+        String sub = s.substring(i,j);
+        if(n%2!=0) {
+            i=j;
+        }
+        while(i>=0 && j<n){
+            //System.out.println(i+", "+j);
+            if(s.charAt(i)==s.charAt(j)){
+                sub = s.substring(i,j+1);
+                //System.out.println(sub);
+                i--;
+                j++;
+            }
+            else {
+                System.out.println("Not matched at : "+i+", "+j);
+                break;
+            }
+        }
+        String lfs = longestPalindromeSubstring(s.substring(0,i+1));
+        String rts = longestPalindromeSubstring(s.substring(j,n));
+
+        if(lfs.length() > rts.length() && lfs.length()>sub.length()) {
+            sub = lfs;
+        }
+        else if(rts.length()>lfs.length() && rts.length()>sub.length()){
+            sub=rts;
+        }
+        return sub;
+    }
     static String longestPalindrome(String S){
         // code here
         String palin = "";

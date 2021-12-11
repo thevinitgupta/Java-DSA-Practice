b681b7871cddf4da823ae0aedf3d399dc04d21cd
Vinit Gupta <vinitman35@gmail.com>
2021-12-11 14:23:19 +0530

    create :Solution for leetcode Q-94 Binary Tree Inorder Traversal

--- src/Trees/BinaryTreeInorderTraversal.java
+++ src/Trees/BinaryTreeInorderTraversal.java
@@ -1,13 +1,19 @@
 package Trees;
 
-import javax.swing.tree.TreeNode;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BinaryTreeInorderTraversal {
-        public List<Integer> inorderTraversal(TreeNode root) {
-            List<Integer> ls = new ArrayList<>();
-            if(root==null) return ls;
-            return ls;
-        }
+    static List<Integer> ino;
+    public List<Integer> inorderTraversal(TreeNode root) {
+        ino = new ArrayList<Integer>();
+        getIno(root);
+        return ino;
+    }
+    private static void getIno(TreeNode root){
+        if(root==null) return;
+        getIno(root.left);
+        ino.add(root.val);
+        getIno(root.right);
+    }
 }
--- src/Trees/TreeNode.java
+++ src/Trees/TreeNode.java
@@ -2,8 +2,8 @@ package Trees;
 
 public class TreeNode {
     public int val;
-    TreeNode left;
-    TreeNode right;
+    public TreeNode left;
+    public TreeNode right;
     TreeNode() {}
     TreeNode(int val) { this.val = val; }
     TreeNode(int val, TreeNode left, TreeNode right) {

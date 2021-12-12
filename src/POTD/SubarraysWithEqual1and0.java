d80dcaec0d375ae5ad430c1d21113cfdcd221a27
Vinit Gupta <vinitman35@gmail.com>
2021-12-12 22:38:36 +0530

    create : O(N*N) POTD by GeeksForGeeks Day-86 Subarrys with equal 0s and 1s(Time limit exceeded)

---
+++ src/POTD/SubarraysWithEqual1and0.java
@@ -0,0 +1,29 @@
+package POTD;
+
+import java.util.HashMap;
+
+public class SubarraysWithEqual1and0 {
+    // O(N*N) - time limit exceeded
+//    static int countSubarrWithEqualZeroAndOne(int arr[], int n)
+//    {
+//        // add your code here
+//        int count = 0,curr = 0;
+//        for(int i=0;i<n-1;i++){
+//            curr = 0;
+//            for(int j=0;j<n;j++){
+//                if(arr[j]==1) curr++;
+//                else curr--;
+//
+//                if(curr==0) count++;
+//            }
+//        }
+//        return count;
+//    }
+
+    static int countSubarrWithEqualZeroAndOne(int arr[], int n)
+    {
+        // add your code here
+
+    }
+
+}

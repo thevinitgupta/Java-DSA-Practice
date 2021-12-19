51f9ad4488df3248aecb4fea9fcdb96b8ecacea7
Vinit Gupta <vinitman35@gmail.com>
2021-12-19 09:27:44 +0530

    create :Solution for leetcode Weekly Contest 272 Q-2 Adding Spaces To A String

---
+++ src/Contests/WeeklyContests/AddingSpacesToAString.java
@@ -0,0 +1,11 @@
+package Contests.WeeklyContests;
+
+public class AddingSpacesToAString {
+    public String addSpaces(String s, int[] spaces) {
+        StringBuilder str = new StringBuilder(s);
+        for(int i=0;i<spaces.length;i++){
+            str.insert(spaces[i]+i," ");
+        }
+        return str.toString();
+    }
+}

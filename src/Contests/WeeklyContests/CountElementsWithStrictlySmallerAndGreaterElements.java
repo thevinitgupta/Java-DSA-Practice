310955c10bd5b4cda81e3f0c4720a0df1fe718c5
Vinit Gupta <vinitman35@gmail.com>
2022-01-23 10:49:22 +0530

    update : Faster Solution Solution for leetcode Weekly Contest 277 Q-1 Count Elements with Strictly Smaller and Greater Elements

--- src/Contests/WeeklyContests/CountElementsWithStrictlySmallerAndGreaterElements.java
+++ src/Contests/WeeklyContests/CountElementsWithStrictlySmallerAndGreaterElements.java
@@ -3,26 +3,38 @@ package Contests.WeeklyContests;
 import java.util.Arrays;
 
 public class CountElementsWithStrictlySmallerAndGreaterElements {
+    //O(NlogN) solution
     public int countElements(int[] nums) {
         Arrays.sort(nums);
         int count = 0;
+        int min = nums[0],max = nums[nums.length-1];
         for(int i=1;i<nums.length-1;i++){
-            boolean smaller = false,larger = false;
-            for(int j = 0;j<i;j++){
-                if(nums[j]<nums[i]) {
-                    smaller = true;
-                    break;
-                }
-            }
-            if(!smaller) continue;
-            for(int j = i+1;j<nums.length;j++){
-                if(nums[j]>nums[i]) {
-                    larger = true;
-                    break;
-                }
-            }
-            if(larger) count++;
+            if(nums[i]>min && nums[i]<max) count++;
         }
         return count;
     }
+
+//    O(N*N) solution
+//    public int countElements(int[] nums) {
+//        Arrays.sort(nums);
+//        int count = 0;
+//        for(int i=1;i<nums.length-1;i++){
+//            boolean smaller = false,larger = false;
+//            for(int j = 0;j<i;j++){
+//                if(nums[j]<nums[i]) {
+//                    smaller = true;
+//                    break;
+//                }
+//            }
+//            if(!smaller) continue;
+//            for(int j = i+1;j<nums.length;j++){
+//                if(nums[j]>nums[i]) {
+//                    larger = true;
+//                    break;
+//                }
+//            }
+//            if(larger) count++;
+//        }
+//        return count;
+//    }
 }

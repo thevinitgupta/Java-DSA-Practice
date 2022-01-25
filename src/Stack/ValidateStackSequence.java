bed06df958f5eaa9b348d581520d36c13f2536c4
Vinit Gupta <vinitman35@gmail.com>
2022-01-25 12:00:35 +0530

    create : Solution for leetcode Q-946 Validate Stack Sequence

---
+++ src/Stack/ValidateStackSequence.java
@@ -0,0 +1,35 @@
+package Stack;
+
+import java.util.Stack;
+
+public class ValidateStackSequence {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        int i=0,j=0,m = pushed.length,n = popped.length;
+        Stack<Integer> st = new Stack<>();
+        st.push(pushed[i]);
+        i++;
+        while(i<m && j<n){
+            if(st.isEmpty()) {
+                st.push(pushed[i]);
+                i++;
+            }
+            if(st.peek()==popped[j]) {
+                st.pop();
+                j++;
+            }
+            else {
+                st.push(pushed[i]);
+                i++;
+            }
+        }
+
+        while(!st.isEmpty() && j<n){
+            if( st.peek()!=popped[j]) {
+                return false;
+            }
+            st.pop();
+            j++;
+        }
+        return true;
+    }
+}

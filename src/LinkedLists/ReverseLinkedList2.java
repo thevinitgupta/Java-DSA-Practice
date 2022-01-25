187aa32a5ba61f9279016a7efb1b541b6d8a3771
Vinit Gupta <vinitman35@gmail.com>
2022-01-25 22:13:16 +0530

    update : Solution for leetcode Q-92 Reverse Linked List II

--- src/LinkedLists/ReverseLinkedList2.java
+++ src/LinkedLists/ReverseLinkedList2.java
@@ -2,30 +2,44 @@ package LinkedLists;
 import static LinkedLists.ReverseLinkedList.reverseList;
 
 public class ReverseLinkedList2 {
+    public static ListNode reverseList(ListNode head) {
+        if(head==null || head.next==null) return head;
+        ListNode prev= null,curr = head,next = head.next;
+        while(curr.next!=null){
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+            next = curr.next;
+        }
+        curr.next = prev;
+        head = curr;
+        return head;
+    }
     public ListNode reverseBetween(ListNode head, int left, int right) {
         if(head.next==null || left==right) return head;
-        ListNode l=head,r=head,temp = head,rev = head;
+        ListNode dHead = new ListNode(-501);
+        dHead.next = head;
+        ListNode temp = head,l = dHead,r = dHead,rev = head;
         int count = 1;
-        while(temp!=null && count<=right){
-            if(count==(left-1)) {
-                l = temp;
-                rev = temp.next;
+        while(temp!=null){
+            if(count<left){
+                l=l.next;
             }
+            else if(count==left) rev = temp;
             if(count==right){
                 r = temp.next;
-                temp.next = null;
-                temp = r;
+                temp.next =null;
+                break;
             }
-            temp = temp.next;
+            temp = temp!=null ? temp.next : null;
             count++;
         }
-        rev = reverseList(rev);
-        temp = rev;
-        l.next = rev;
-        while(temp.next!=null){
+        l.next = reverseList(rev);
+        temp = l.next;
+        while(temp.next!=null) {
             temp = temp.next;
         }
         temp.next = r;
-        return head;
+        return dHead.next;
     }
 }

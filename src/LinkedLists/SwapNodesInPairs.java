c48622494aecf7cbba65b5d162305de8497a2329
Vinit Gupta <vinitman35@gmail.com>
2022-02-01 12:52:27 +0530

    update : Solution for leetcode Q-24 Swap Nodes in Pairs

--- src/LinkedLists/SwapNodesInPairs.java
+++ src/LinkedLists/SwapNodesInPairs.java
@@ -1,6 +1,22 @@
 package LinkedLists;
 
 public class SwapNodesInPairs {
+    public ListNode swapPair(ListNode head) {
+        if(head==null) return null;
+        ListNode dHead = new ListNode(-1);
+        dHead.next = head;
+        ListNode prev = dHead, curr = head;
+        ListNode temp = head.next;
+        while(temp!=null){
+            prev.next = temp;
+            curr.next = temp.next;
+            temp.next = curr;
+            prev = curr;
+            curr = curr.next;
+            temp = curr==null? null : curr.next;
+        }
+        return dHead.next;
+    }
     public ListNode swapPairs(ListNode head) {
         if(head==null || head.next==null) return head;
         ListNode odd = head,even = head.next,prev = head;

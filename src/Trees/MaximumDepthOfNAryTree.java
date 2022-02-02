0ed7f45da4cf4913f1455e257ac7407ed680454a
Vinit Gupta <vinitman35@gmail.com>
2022-02-02 17:06:06 +0530

    create : Solution for leetcode Q-559 Maximum Depth of N-Ary Tree

--- src/Trees/MaximumDepthOfNAryTree.java
+++ src/Trees/MaximumDepthOfNAryTree.java
@@ -2,5 +2,17 @@ package Trees;
 
 import java.util.List;
 public class MaximumDepthOfNAryTree {
-
+    public int maxDepth(NNode root) {
+        return getMax(root,1,1);
+    }
+    public static int getMax(NNode root, int curr,int max){
+        if(root==null) return 0;
+        max = Math.max(curr,max);
+        curr++;
+        List<NNode> children = root.children;
+        for(int i=0;i<children.size();i++){
+            max = Math.max(max,  getMax(children.get(i),curr,max));
+        }
+        return max;
+    }
 }

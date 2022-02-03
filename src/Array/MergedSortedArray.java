aed8b3e28202a84df33bfca579f9418b19c7d67a
Vinit Gupta <vinitman35@gmail.com>
2022-02-03 20:23:58 +0530

    update : Faster Solution for leetcode Q-88 Merge Sorted Array

--- src/Array/MergedSortedArray.java
+++ src/Array/MergedSortedArray.java
@@ -1,6 +1,25 @@
 package Array;
 
 public class MergedSortedArray {
+    public void mergeOpt(int[] nums1, int m, int[] nums2, int n){
+        int k= m+n-1, i=m-1,j = n-1;
+        while(i>=0 && j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[k] = nums1[i];
+                i--;
+            }
+            else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+        while(j>=0){
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int i=0,j=0;
         while(i<m && j<n){

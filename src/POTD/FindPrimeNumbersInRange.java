3c57e9fe03002b97b51cfc16dc453554aa7df6ab
Vinit Gupta <vinitman35@gmail.com>
2022-02-08 16:51:11 +0530

    create : POTD by GeeksForGeeks Day-144 Prime Numbers in a Range

---
+++ src/POTD/FindPrimeNumbersInRange.java
@@ -0,0 +1,23 @@
+package POTD;
+
+import java.util.ArrayList;
+
+public class FindPrimeNumbersInRange {
+    public boolean isprime(int n){
+        if(n==1 || n==0)
+            return false;
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n%i==0)
+                return false;
+        }
+        return true;
+    }
+    ArrayList<Integer> o1 = new ArrayList<>();
+    ArrayList<Integer> primeRange(int M, int N) {
+        for(int i=M;i<=N;i++){
+            if(isprime(i))
+                o1.add(i);
+        }
+        return o1;
+    }
+}

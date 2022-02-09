c61e0dca4522b5511b2fc35cfc79f48ea032f8bb
Vinit Gupta <vinitman35@gmail.com>
2022-02-09 10:18:55 +0530

    update : POTD by GeeksForGeeks Day-145 Sort by set bit count

--- src/POTD/SortBySetBitCount.java
+++ src/POTD/SortBySetBitCount.java
@@ -9,4 +9,13 @@ public class SortBySetBitCount {
             return (Integer.bitCount(b)-Integer.bitCount(a));
         });
     }
+    static int setBits(Integer n)
+    {
+        int count = 0;
+        while (n > 0) {
+            count += n & 1;
+            n >>= 1;
+        }
+        return count;
+    }
 }

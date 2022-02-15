e14f09a798ba552edc37216fb2f8710c953d3243
Vinit Gupta <vinitman35@gmail.com>
2022-02-15 19:50:31 +0530

    create : Solution for leetcode Q-121 Best Time To Buy And Sell Stock

---
+++ src/DynamicProgramming/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,12 @@
+package DynamicProgramming;
+
+public class BestTimeToBuyAndSellStock {
+    public int maxProfit(int[] prices) {
+        int profit = 0,min = prices[0];
+        for(int i=1;i<prices.length;i++){
+            min = Math.min(min, prices[i]);
+            profit = Math.max(profit, prices[i]-min);
+        }
+        return profit;
+    }
+}

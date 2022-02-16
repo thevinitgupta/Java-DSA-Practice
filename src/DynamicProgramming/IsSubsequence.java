14442ac9ad49f38efbfa3c8242977f8a748e4acc
Vinit Gupta <vinitman35@gmail.com>
2022-02-16 12:23:50 +0530

    create : Solution for leetcode Q-392 Is Subsequence

---
+++ src/DynamicProgramming/IsSubsequence.java
@@ -0,0 +1,12 @@
+package DynamicProgramming;
+
+public class IsSubsequence {
+    public boolean isSubsequence(String s, String t) {
+        int i=0,j=0,m = s.length(), n = t.length();
+        while(i<m && j<n){
+            if(s.charAt(i) == t.charAt(j)) i++;
+            j++;
+        }
+        return i>=m;
+    }
+}

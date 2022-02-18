e8c6dd8d1cec90596820bc9c8ea739dd9797bce5
Vinit Gupta <vinitman35@gmail.com>
2022-02-18 10:05:14 +0530

    create : POTD by GeeksForGeeks Day-154 Rearrange Geek and His Classmates

---
+++ src/POTD/RearrangeGeekAndHisClassmates.java
@@ -0,0 +1,14 @@
+package POTD;
+
+public class RearrangeGeekAndHisClassmates {
+    void prank(long[] a, int n)
+    {
+        long N = n;
+        for(int i = 0;i<n;i++){
+            a[i] = a[i] + (a[(int )a[i]]%N) * N;
+        }
+
+        for(int i = 0;i<n;i++)
+            a[i] = a[i]/N;
+    }
+}

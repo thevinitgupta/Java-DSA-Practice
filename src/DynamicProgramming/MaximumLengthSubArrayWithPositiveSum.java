972ddf4ebe8c2e4c8ceb51d63113ad0f93ed9f04
Vinit Gupta <vinitman35@gmail.com>
2022-03-04 13:08:21 +0530

    create : Solution for leetcode Q-1567 Maximum Length SUbarray with Positive SUm with DP

--- src/DynamicProgramming/MaximumLengthSubArrayWithPositiveSum.java
+++ src/DynamicProgramming/MaximumLengthSubArrayWithPositiveSum.java
@@ -2,39 +2,61 @@ package DynamicProgramming;
 
 public class MaximumLengthSubArrayWithPositiveSum {
 
-    //TLE
     public int getMaxLen(int[] nums) {
-        int prev = 0,n = nums.length,maxLen= 0;
-        long prod = 1;
-        if(n==1) {
-            if(nums[0]>0) return 1;
-            else return 0;
-        }
-        for(int i=0;i<n;i++){
-            if(nums[i]==0){
-                if(i==prev){
-                    prod = 1;
-                    prev = i+1;
-                    continue;
-                }
-                if(prod>0) maxLen = Math.max(maxLen,i-prev);
-                else {
-                    maxLen = Math.max(maxLen, getMax(nums,prod, prev, i-1));
-                    prod = 1;
-                }
-                prev = i+1;
+        int n = nums.length;
+        int[] pos = new int[n];
+        int[] neg = new int[n];
+        if (nums[0] > 0) pos[0] = 1;
+        if (nums[0] < 0) neg[0] = 1;
+        int ans = pos[0];
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > 0) {
+                pos[i] = 1 + pos[i - 1];
+                neg[i] = neg[i - 1] > 0 ? 1 + neg[i - 1]:0;
+            } else if (nums[i] < 0) {
+                pos[i] = neg[i - 1] > 0 ? 1 + neg[i - 1]:0;
+                neg[i] = 1 + pos[i - 1];
             }
-            prod = nums[i]==0 ? 1 : (prod * nums[i]) % 1000000007;
+            ans = Math.max(ans, pos[i]);
         }
-        maxLen = Math.max(maxLen,getMax(nums, prod, prev, n-1));
-        return maxLen;
+        return ans;
     }
-    public static int getMax(int [] nums, long prod, int low, int high){
-        if(low>=high) return 0;
-        if(prod>0) return (high-low)+1;
-        long pre = prod / nums[low];
-        long post = prod / nums[high];
-        if(pre>0 || post>0) return high-low;
-        return Math.max(getMax(nums, pre, low+1, high), getMax(nums, post, low, high-1));
-    }
+
+    //TLE
+//    public int getMaxLen(int[] nums) {
+//        int prev = 0,n = nums.length,maxLen= 0;
+//        long prod = 1;
+//        if(n==1) {
+//            if(nums[0]>0) return 1;
+//            else return 0;
+//        }
+//        for(int i=0;i<n;i++){
+//            if(nums[i]==0){
+//                if(i==prev){
+//                    prod = 1;
+//                    prev = i+1;
+//                    continue;
+//                }
+//                if(prod>0) maxLen = Math.max(maxLen,i-prev);
+//                else {
+//                    maxLen = Math.max(maxLen, getMax(nums,prod, prev, i-1));
+//                    prod = 1;
+//                }
+//                prev = i+1;
+//            }
+//            prod = nums[i]==0 ? 1 : (prod * nums[i]) % 1000000007;
+//        }
+//        if(prod<0)
+//        maxLen = Math.max(maxLen,getMax(nums, prod, prev, n-1));
+//        else maxLen = Math.max(maxLen, n-prev);
+//        return maxLen;
+//    }
+//    public static int getMax(int [] nums, long prod, int low, int high){
+//        if(low>=high) return 0;
+//        if(prod>0) return (high-low)+1;
+//        long pre = prod / nums[low];
+//        long post = prod / nums[high];
+//        if(pre>0 || post>0) return high-low;
+//        return Math.max(getMax(nums, pre, low+1, high), getMax(nums, post, low, high-1));
+//    }
 }

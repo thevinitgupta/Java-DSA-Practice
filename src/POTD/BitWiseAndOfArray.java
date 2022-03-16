88dc33af4bb4fc9608e8318aba97ff1eb2fac6f8
Vinit Gupta <vinitman35@gmail.com>
2022-03-16 09:40:01 +0530

    create : POTD by GeeksForGeeks Day-182 Bit Wise And of Array

---
+++ src/POTD/BitWiseAndOfArray.java
@@ -0,0 +1,22 @@
+package POTD;
+
+public class BitWiseAndOfArray {
+    int count(int N, int A[], int X)
+    {
+        int prefix=0,ans=N;
+        for(int i=30;i>=0;i--){
+            if(((X>>i)&1)!=0){
+                prefix^=(1l<<i);
+                continue;
+            }
+            int ct=0;
+            long p=prefix^(1l<<i);
+            for(int j=0;j<N;j++){
+                if((A[j]&p)==p)
+                    ct++;
+            }
+            ans=Math.min(ans,N-ct);
+        }
+        return ans;
+    }
+}

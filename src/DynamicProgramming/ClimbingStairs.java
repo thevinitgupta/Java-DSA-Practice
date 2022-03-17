444675cdec7ed5a63d0f8d8fb28ef39ffd60736c
Vinit Gupta <vinitman35@gmail.com>
2022-03-17 10:39:02 +0530

    create : Solution for leetcode Q-70 Climbing Stairs Iterative

--- src/DynamicProgramming/ClimbingStairs.java
+++ src/DynamicProgramming/ClimbingStairs.java
@@ -12,4 +12,15 @@ public class ClimbingStairs {
         dp[n] = getWays(n-1,dp) + getWays(n-2,dp);
         return dp[n];
     }
+    public int climbStairsIterative(int n) {
+        int [] dp = new int[n+1];
+        dp[0]= 1;
+        dp[1] = 1;
+        int k=2;
+        while(dp[n]==0){
+            dp[k] = dp[k-1]+dp[k-2];
+            k++;
+        }
+        return dp[n];
+    }
 }

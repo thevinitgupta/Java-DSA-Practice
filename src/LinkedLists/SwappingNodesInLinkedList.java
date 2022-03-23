4c5ff9de67ba90100b77d7e73124c9d46fd8a30a
Vinit Gupta <vinitman35@gmail.com>
2022-03-23 09:50:43 +0530

    create : Solution for leetcode Q-1721 Swapping Nodes in Linked Lists

--- src/LinkedLists/SwappingNodesInLinkedList.java
+++ src/LinkedLists/SwappingNodesInLinkedList.java
@@ -29,4 +29,38 @@ public class SwappingNodesInLinkedList {
         }
         return head;
     }
+
+    // swap without changing values
+    public ListNode swapNodes2(ListNode head, int K) {
+        int num = getSize(head);
+        if(K>num) return head;
+        int pos = num-(K-1), curr = 1;
+        ListNode dHead = new ListNode(-1);
+        dHead.next = head;
+        ListNode temp = head, prev = dHead, first = null, second = null;
+        while(temp!=null){
+            if(curr==K) {
+                first = prev;
+            }
+            if(curr==pos) {
+                second = prev;
+            }
+            prev = temp;
+            temp = temp.next;
+            curr++;
+        }
+        ListNode fn = first.next, fnn = fn.next, sn = second.next;
+        if(num%2==0 && K==num/2){
+            ListNode snn = sn==null? null : sn.next;
+            first.next = sn;
+            sn.next = fn;
+            fn.next = snn;
+            return dHead.next;
+        }
+        first.next = sn;
+        fn.next = sn.next;
+        sn.next = fnn;
+        second.next = fn;
+        return dHead.next;
+    }
 }

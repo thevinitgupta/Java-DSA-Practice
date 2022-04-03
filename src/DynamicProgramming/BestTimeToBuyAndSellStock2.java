031c74978546ebd1f5924dcde2d6105b0a4e07eb
Vinit Gupta <vinitman35@gmail.com>
2022-04-03 17:42:32 +0530

    create : Solution for leetcode Q-122 Best Time To Buy and Sell Stocks II (Different Approach)

--- src/DynamicProgramming/BestTimeToBuyAndSellStock2.java
+++ src/DynamicProgramming/BestTimeToBuyAndSellStock2.java
@@ -1,6 +1,19 @@
 package DynamicProgramming;
 
 public class BestTimeToBuyAndSellStock2 {
+    public int maxProfit2(int[] prices) {
+        int b = 0, s = 0, n = prices.length, curr = 0, tot = 0;
+        while(curr< n && b<n && s<n){
+            //buy a stco kand hold it until the price starts dropping and sell on the day before the drop
+            while(curr<n && prices[curr]>=prices[s]){
+                s = curr;
+                curr++;
+            }
+            tot += prices[s]- prices[b];
+            b = curr; s = curr;
+        }
+        return tot;
+    }
     public int maxProfit(int[] prices) {
         int max = 0, prev = prices[0];
         boolean bought = true;

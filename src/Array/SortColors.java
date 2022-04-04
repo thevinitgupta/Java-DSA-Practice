77d26a709d2a00e622e34c319bb85d9bec63c804
Vinit Gupta <vinitman35@gmail.com>
2022-04-04 07:45:30 +0530

    create : Solution for leetcode Q-75 Sort Colors

--- src/Array/SortColors.java
+++ src/Array/SortColors.java
@@ -2,6 +2,29 @@ package Array;
 
 public class SortColors {
     public void sortColors(int[] nums) {
+        int lo = 0, mid = 0, high = nums.length-1;
+        while(mid<=high){
+            if(nums[mid]<1) {
+                int temp = nums[lo];
+                nums[lo] = nums[mid];
+                nums[mid] = temp;
+                lo++;
+                mid++;
+            }
+            else if(nums[mid]>1) {
+                int temp = nums[high];
+                nums[high] = nums[mid];
+                nums[mid] = temp;
+                high--;
+            }
+            else {
+                mid++;
+            }
+            // System.out.println(Arrays.toString(nums));
+            // System.out.println("low="+lo+", mid="+mid+", high="+high);
+        }
+    }
+    public void sortColorsCopied(int[] nums) {
         int low = 0,mid = 0,high = nums.length;
         while(mid<high){
             if(nums[high]==2) high--;

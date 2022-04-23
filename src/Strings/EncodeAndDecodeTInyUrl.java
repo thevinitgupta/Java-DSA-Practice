410dc22668e3ef47c0d22b6234f0ad4b043e3950
Vinit Gupta <vinitman35@gmail.com>
2022-04-23 10:27:48 +0530

    create : Solution for leetcode Q-535 Encode and Decode Tiny Url

---
+++ src/Strings/EncodeAndDecodeTInyUrl.java
@@ -0,0 +1,12 @@
+package Strings;
+
+public class EncodeAndDecodeTInyUrl {
+    public String encode(String longUrl) {
+        return longUrl;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return shortUrl;
+    }
+}

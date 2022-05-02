9253af12ac6851f7201cf261476303954ebd44e2
Vinit Gupta <vinitman35@gmail.com>
2022-05-02 18:19:40 +0530

    update : Solution for leetcode Q-42 Trapping Rainwater with DP

--- src/DynamicProgramming/TrappingRainwater.java
+++ src/DynamicProgramming/TrappingRainwater.java
@@ -2,6 +2,28 @@ package DynamicProgramming;
 
 public class TrappingRainwater {
     public int trap(int[] height) {
+        int n = height.length;
+        int [] rMax = new int[n];
+        int [] lMax = new int[n];
+        rMax[n-1] = -1;
+        lMax[0] = -1;
+
+        for(int i=n-2;i>=0;i--){
+            rMax[i] = Math.max(rMax[i+1], height[i+1]);
+        }
+        for(int i=1;i<n;i++){
+            lMax[i] = Math.max(lMax[i-1], height[i-1]);
+        }
+        int amt = 0;
+        for(int i=0;i<n;i++){
+            int cMin = Math.min(lMax[i],rMax[i]);
+            if(cMin>height[i]){
+                amt += cMin-height[i];
+            }
+        }
+        return amt;
+    }
+    public int trap2(int[] height) {
         int amt = 0;
         for(int i=1;i<height.length;i++){
             int left = i-1, right = i+1,lMax = height[i], rMax = height[i];

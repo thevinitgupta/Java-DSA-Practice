8027871ffa019e26a62fa06f8bb62de59423dee1
Vinit Gupta <vinitman35@gmail.com>
2022-05-09 12:05:31 +0530

    InfyTQ Prep : 0/1 Knapsack with Dp

--- src/InfyTQPrep/Knapsack01.java
+++ src/InfyTQPrep/Knapsack01.java
@@ -1,6 +1,25 @@
 package InfyTQPrep;
 
 public class Knapsack01 {
+
+    //dp solution
+    static int knapSack(int W, int wt[], int val[], int n)
+    {
+        int [][] dp = new int[n][W+1];
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=W;j++){
+                if(j-wt[i]>=0){
+                    dp[i][j] = Math.max(i-1>=0 ? dp[i-1][j] : 0, (i-1>=0 ? dp[i-1][j-wt[i]] : 0)+val[i]);
+                }
+                else dp[i][j] = i-1>=0 ? dp[i-1][j] : 0;
+            }
+        }
+
+        return dp[n-1][W];
+    }
+
+
     static int knapSackRecursive(int W, int wt[], int val[], int n)
     {
          return Math.max(getMax(W,wt,val,0,1,n), getMax(W-wt[0],wt,val,val[0],1,n));

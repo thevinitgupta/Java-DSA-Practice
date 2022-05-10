1f9ff8e62cd92d6c8bd682b8f288bc3447a46cb5
Vinit Gupta <vinitman35@gmail.com>
2022-05-10 10:43:17 +0530

    InfyTQ Prep : Heap Sort - Sorting

--- src/InfyTQPrep/HeapSort.java
+++ src/InfyTQPrep/HeapSort.java
@@ -3,7 +3,7 @@ package InfyTQPrep;
 import java.util.Arrays;
 
 public class HeapSort {
-    public static int [] insert(int [] arr, int val, int pos){
+    private static int [] insert(int [] arr, int val, int pos){
         arr[pos] = val;
         while(pos/2>0 && arr[pos]>arr[pos/2]){
             arr[pos] = arr[pos/2];
@@ -12,7 +12,7 @@ public class HeapSort {
         }
         return arr;
     }
-    public static void buildHeap(int [] arr, int n){
+    private static void buildHeap(int [] arr, int n){
         int [] heap = new int[n];
         for(int i=1;i<n;i++){
             heap = insert(arr,arr[i], i);
@@ -20,9 +20,39 @@ public class HeapSort {
         arr = heap;
     }
 
+    private static void delete(int arr[], int last){
+        int del = arr[1], i=1;
+        arr[1] = arr[last];
+        while(i*2+1<last){
+            int lc = i*2, rc = (i*2)+1;
+            if(arr[lc]>arr[rc]){
+                int temp = arr[lc];
+                arr[lc] = arr[i];
+                arr[i] = temp;
+                i=lc;
+            }
+            else {
+                int temp = arr[rc];
+                arr[rc] = arr[i];
+                arr[i] = temp;
+                i=rc;
+            }
+        }
+        arr[last] = del;
+    }
+
+    public static void sort(int [] arr, int n){
+        int end = n-1;
+        for(;end>1;end--){
+            delete(arr,end);
+        }
+    }
+
     public static void main(String[] args) {
         int [] arr = {-1,10,20,15,30,40};
         buildHeap(arr,arr.length);
+        System.out.println(Arrays.toString(arr));
+        sort(arr,arr.length);
         System.out.println(Arrays.toString(arr));
     }
 }

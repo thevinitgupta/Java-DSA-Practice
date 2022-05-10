4fa8452c03da3f4ee4662a86abc3cf92de2dbd59
Vinit Gupta <vinitman35@gmail.com>
2022-05-10 11:52:28 +0530

    InfyTQ Prep : Heap Sort - Sorting using Heapify

--- src/InfyTQPrep/HeapSort.java
+++ src/InfyTQPrep/HeapSort.java
@@ -41,17 +41,47 @@ public class HeapSort {
         arr[last] = del;
     }
 
+    //simple insertion / deletion sort
+//    public static void sort(int [] arr, int n){
+//        int end = n-1;
+//        for(;end>1;end--){
+//            delete(arr,end);
+//        }
+//    }
+
     public static void sort(int [] arr, int n){
+        for(int i=n/2;i>=1;i--){
+            maxHeapify(arr,i,n);
+        }
+        System.out.println(Arrays.toString(arr));
         int end = n-1;
-        for(;end>1;end--){
-            delete(arr,end);
+        for(;end>=1;end--){
+            int temp = arr[end];
+            arr[end] = arr[1];
+            arr[1] = temp;
+            maxHeapify(arr,1,end);
+        }
+    }
+
+    private static void maxHeapify(int [] arr, int pos, int end){
+        int l = pos*2, r = pos*2 + 1, largest = pos;
+        if(l<end && arr[l]>arr[pos]){
+            largest = l;
+        }
+        if(r<end && arr[r]>arr[largest]){
+            largest = r;
+        }
+        if(largest!=pos){
+            int temp = arr[pos];
+            arr[pos] = arr[largest];
+            arr[largest] = temp;
+            maxHeapify(arr,largest,end);
         }
     }
 
     public static void main(String[] args) {
-        int [] arr = {-1,10,20,15,30,40};
-        buildHeap(arr,arr.length);
-        System.out.println(Arrays.toString(arr));
+        int [] arr = {-1,10,20,15,1001,30,40,0};
+//        System.out.println(Arrays.toString(arr));
         sort(arr,arr.length);
         System.out.println(Arrays.toString(arr));
     }

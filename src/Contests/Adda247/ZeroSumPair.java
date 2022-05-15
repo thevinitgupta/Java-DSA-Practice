1406b3cd60ad313037424fbf0d6d627c35d28a92
Vinit Gupta <vinitman35@gmail.com>
2022-05-15 23:58:36 +0530

    create : POTD by GeeksForGeeks Day-224 Unique Subsets

--- src/Contests/Adda247/ZeroSumPair.java
+++ src/Contests/Adda247/ZeroSumPair.java
@@ -29,4 +29,6 @@ public class ZeroSumPair {
 
         return count;
     }
+
+    //
 }
---
+++ src/POTD/UniqueSubsets.java
@@ -0,0 +1,23 @@
+package POTD;
+import java.util.*;
+
+public class UniqueSubsets {
+    public static ArrayList <ArrayList <Integer>> AllSubsets(int arr[], int n)
+    {
+        Arrays.sort(arr);
+        ArrayList<ArrayList<Integer>> ans=new ArrayList<>();
+        getSet(arr,new ArrayList<Integer>(),n,ans,0);
+        ans.remove(0);
+        return ans;
+    }
+    public static void getSet(int []arr,ArrayList<Integer> ls,int n,ArrayList<ArrayList<Integer>> ans,
+                              int ind){
+        ans.add(new ArrayList<>(ls));
+        for(int i=ind;i<n;i++){
+            if(i!=ind&&arr[i]==arr[i-1]) continue;
+            ls.add(arr[i]);
+            getSet(arr,ls,n,ans,i+1);
+            ls.remove(ls.size()-1);
+        }
+    }
+}

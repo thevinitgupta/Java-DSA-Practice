c95d3141df8b9ad0f43e1c8076fca4c1f2bcee76
Vinit Gupta <vinitman35@gmail.com>
2022-05-25 11:00:50 +0530

    create : GeeksForGeeks Q Subset Sum Problem With Dp

--- src/DynamicProgramming/SubsetSumProblem.java
+++ src/DynamicProgramming/SubsetSumProblem.java
@@ -4,6 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SubsetSumProblem {
+    static Boolean isSubsetSumDP(int n, int set[], int sum){
+        boolean subset[][] = new boolean[sum + 1][n + 1];
+
+        // If sum is 0, then answer is true
+        for (int i = 0; i <= n; i++)
+            subset[0][i] = true;
+
+        // If sum is not 0 and set is empty,
+        // then answer is false
+        for (int i = 1; i <= sum; i++)
+            subset[i][0] = false;
+
+        // Fill the subset table in bottom
+        // up manner
+        for (int i = 1; i <= sum; i++) {
+            for (int j = 1; j <= n; j++) {
+                subset[i][j] = subset[i][j - 1];
+                if (i >= set[j - 1])
+                    subset[i][j] = subset[i][j]
+                            || subset[i - set[j - 1]][j - 1];
+            }
+        }
+        return subset[sum][n];
+    }
     static Boolean isSubsetSum(int N, int arr[], int sum){
         List<Integer> sums = new ArrayList<>();
         List<List<Integer>> subsets = subsets(arr,sums);

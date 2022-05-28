e5ca7338709344452280d4bfe6f9537dc2461bfc
Vinit Gupta <vinitman35@gmail.com>
2022-05-28 11:50:18 +0530

    create : POTD by GeeksForGeeks Day-232 Shortest Path Between Cities

---
+++ src/POTD/ShortestPathBetweenCities.java
@@ -0,0 +1,16 @@
+package POTD;
+
+public class ShortestPathBetweenCities {
+    int shortestPath( int x, int y){
+        // code here
+        int ans =0;
+        while(x!=y){
+            if(x>y)
+                x/=2;
+            else
+                y/=2;
+            ans++;
+        }
+        return ans;
+    }
+}

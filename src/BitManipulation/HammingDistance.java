d4cb7a10499cf894e43d55fcbca77728179f27f0
Vinit Gupta <vinitman35@gmail.com>
2022-06-14 11:48:33 +0530

    Create : LeetCode Q-461 Hamming Distance

--- src/BitManipulation/HammingDistance.java
+++ src/BitManipulation/HammingDistance.java
@@ -2,6 +2,15 @@ package BitManipulation;
 
 public class HammingDistance {
     public int hammingDistance(int x, int y) {
+        int xor = x^y;
+        int count = 0;
+        while(xor!=0){
+            xor &= xor-1;
+            count++;
+        }
+        return count;
+    }
+    public int hammingDistanceOld(int x, int y) {
         int z = x^y;
         int count = 0;
         while(z>0){

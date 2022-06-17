b1a341a90160468d92d2a6dd46b2933aed05ce92
Vinit Gupta <vinitman35@gmail.com>
2022-06-17 12:29:31 +0530

    Create : LeetCode Q-67 Add Binary

--- src/BitManipulation/AddBinary.java
+++ src/BitManipulation/AddBinary.java
@@ -2,6 +2,20 @@ package BitManipulation;
 
 public class AddBinary {
     public String addBinary(String a, String b) {
+        StringBuilder s = new StringBuilder();
+        int i=a.length()-1,j=b.length()-1, carry = 0;
+
+        while(i>=0 || j>=0){
+            int d1 = i>=0 ? a.charAt(i) - '0' : 0, d2 = j>=0 ? b.charAt(j) - '0' : 0;
+            int curr = d1 ^ d2 ^ carry;
+            s.append(curr);
+            carry = d1&d2 | d1&carry | d2&carry;;
+            i--;j--;
+        }
+        if(carry!=0) s.append(carry);
+        return s.reverse().toString();
+    }
+    public String addBinaryString(String a, String b) {
         StringBuilder str = new StringBuilder();
         int i=a.length()-1, j = b.length()-1,carry = 0;
         while(i>=0 || j>=0){

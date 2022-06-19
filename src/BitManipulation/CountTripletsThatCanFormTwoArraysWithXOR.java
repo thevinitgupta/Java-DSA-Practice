c5c4f079c43c1111ca77a02b65fb1b07d3edc02b
Vinit Gupta <vinitman35@gmail.com>
2022-06-19 13:07:04 +0530

    Create : LeetCode Q-1442 Count Triplets That Can Form Two Arrays With Equal XOR

---
+++ src/BitManipulation/CountTripletsThatCanFormTwoArraysWithXOR.java
@@ -0,0 +1,16 @@
+package BitManipulation;
+
+public class CountTripletsThatCanFormTwoArraysWithXOR {
+    public int countTriplets(int[] arr) {
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            int xor=arr[i];
+            for(int j=i+1;j<arr.length;j++){
+                xor ^= arr[j];
+                if(xor==0) count += (j-i);
+            }
+        }
+
+        return count;
+    }
+}

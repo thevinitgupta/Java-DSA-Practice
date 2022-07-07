e8c399eaf73ba3c6d0e9467c0fb35bcadac7100a
Vinit Gupta <vinitman35@gmail.com>
2022-07-07 23:27:58 +0530

    Create : Interviewbit Q Length Of Last Word

---
+++ src/InterviewBit/Strings/LengthOfLastWord.java
@@ -0,0 +1,17 @@
+package InterviewBit.Strings;
+
+public class LengthOfLastWord {
+    public int lengthOfLastWord(final String A) {
+        int idx = A.length() - 1;
+        while (idx >= 0 && A.charAt(idx) == ' ') {
+            idx--;
+        }
+        int count = 0;
+        while (idx >= 0 && A.charAt(idx) != ' ') {
+            idx--;
+            count++;
+        }
+        return count;
+    }
+}
+}

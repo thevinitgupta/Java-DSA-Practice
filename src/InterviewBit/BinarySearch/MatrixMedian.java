1de8eea7745b8812a51c74e05cbadeb6e4a88a60
Vinit Gupta <vinitman35@gmail.com>
2022-07-12 19:38:39 +0530

    Create : Interviewbit Q Matrix Median (Custom Sorting

--- src/InterviewBit/BinarySearch/MatrixMedian.java
+++ src/InterviewBit/BinarySearch/MatrixMedian.java
@@ -3,6 +3,38 @@ package InterviewBit.BinarySearch;
 import java.util.ArrayList;
 
 public class MatrixMedian {
+    //my sol
+    /*public int findMedian(ArrayList<ArrayList<Integer>> A) {
+        int i=1,j=i-1, m = A.size(), n = A.get(0).size();
+        Collections.sort(A,new FirstElement());
+        while(i<m){
+            int l1 = 0, l2 = 0;
+            ArrayList<Integer> top = A.get(j), bottom = A.get(i);
+            while(l1<n && l2<n){
+                if(bottom.get(l2)<top.get(l1)){
+                    int temp = top.get(l1);
+                    top.set(l1,bottom.get(l2));
+                    while(l2+1<n && bottom.get(l2+1)<temp) l2++;
+                    bottom.set(l2,temp);
+                }
+                l1++;
+            }
+            i++;
+            j++;
+        }
+        for(i=0;i<m;i++){
+            System.out.println(A.get(i));
+        }
+        int tot = n*m, median = tot/2;
+        for(i=0;i<m;i++){
+            for(j=0;j<n;j++){
+                if(median==0) return A.get(i).get(j);
+                median--;
+            }
+        }
+        return 0;
+    }
+    */
     public int findMedian(ArrayList<ArrayList<Integer>> A) {
         int n = A.size();
         if(n == 0) return -1;

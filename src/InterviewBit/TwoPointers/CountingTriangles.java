236b4c655adb0dbe337b84b16fcb78e45d69e1af
Vinit Gupta <vinitman35@gmail.com>
2022-07-20 22:35:11 +0530

    Create : Interviewbit Q Counting Triangles

---
+++ src/InterviewBit/TwoPointers/CountingTriangles.java
@@ -0,0 +1,31 @@
+package InterviewBit.TwoPointers;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class CountingTriangles {
+    public int nTriang(ArrayList<Integer> A) {
+        int n = A.size();
+        int res = 0;
+        if (n < 3) return res;
+        int mod = 1000000007;
+        Collections.sort(A);
+
+        for(int i=n-1; i>=0; i--) {
+            int j = 0;
+            int k = i-1;
+            while (j<k) {
+                long sum = A.get(j) + A.get(k);
+                if (sum > A.get(i)) {
+                    res = (res + (k-j)%mod)%mod;
+                    k--;
+                }
+                else {
+                    j++;
+                }
+            }
+        }
+
+        return res;
+    }
+}

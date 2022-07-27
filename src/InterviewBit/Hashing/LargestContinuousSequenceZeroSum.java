8b68827ae030447c5333dd4a0286aa3691eea0c7
Vinit Gupta <vinitman35@gmail.com>
2022-07-27 21:58:05 +0530

    Create : Interviewbit Q Largest Continuous Sequence Zero Sum

---
+++ src/InterviewBit/Hashing/LargestContinuousSequenceZeroSum.java
@@ -0,0 +1,42 @@
+package InterviewBit.Hashing;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LargestContinuousSequenceZeroSum {
+    public ArrayList<Integer> lszero(ArrayList<Integer> A) {
+        ArrayList<Integer> sumList = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        ArrayList<Integer> result = new ArrayList<Integer>();
+
+        map.put(0,-1);
+
+        int start = -1;
+        int end = -1;
+        int sum = 0;
+        int maxLen = -1;
+
+        for (int i=0;i<A.size();i++) {
+            sum += A.get(i);
+            if (map.containsKey(sum)) {
+                if (maxLen < (i - map.get(sum))) {
+                    start = map.get(sum) + 1;
+                    end = i;
+                    maxLen = i - map.get(sum);
+                }
+            }
+            else {
+                map.put(sum, i);
+            }
+        }
+
+        if(start >= 0 && end >= 0) {
+            for(int i = start; i <= end; i++) {
+                result.add(A.get(i));
+            }
+        }
+
+        return result;
+    }
+}

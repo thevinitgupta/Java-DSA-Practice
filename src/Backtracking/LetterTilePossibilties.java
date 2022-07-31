a76044469b4bf5933a1d28fa0b64188430d871fc
Vinit Gupta <vinitman35@gmail.com>
2022-07-31 22:29:00 +0530

    Create : Leetcode Q-1079 Letter Tile Possibiliteies

---
+++ src/Backtracking/LetterTilePossibilties.java
@@ -0,0 +1,23 @@
+package Backtracking;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LetterTilePossibilties {
+    public int numTilePossibilities(String tiles) {
+        Set<String> set = new HashSet<>();
+        boolean[] vis = new boolean[tiles.length()];
+        permute(tiles, "", set, vis);
+        return set.size()-1;
+    }
+    public void permute(String tiles, String curr, Set<String> set, boolean[] vis){
+        set.add(curr);
+        for(int i=0; i<tiles.length(); i++){
+            if(!vis[i]){
+                vis[i]=true;
+                permute(tiles, curr+tiles.charAt(i), set, vis);
+                vis[i]=false;
+            }
+        }
+    }
+}
